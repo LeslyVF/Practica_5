1542d156e6a57384855049e45ac905c4256855a3
armentaspcs <armentaspcs@DESKTOP-AHUAJBN>
2022-09-28 14:02:36 -0700

    Agregar archivos de proyecto.

---
+++ Estacionamiento/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamiento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double min;
+
+            Console.WriteLine("Dame los minutos:");
+            min = Convert.ToDouble(Console.ReadLine());
+
+            if (min <= 60)
+            {
+                Console.WriteLine("Pagas $5 pesos");
+
+            }
+            if (min >= 61 & min <= 120)
+            {
+                Console.WriteLine("Pagas $15 pesos");
+
+            }
+            if (min > 121)
+            {
+                Console.WriteLine("Pagas $40 pesos");
+
+            }
+        }
+    }
+}
---
+++ Par o impar/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Par_o_impar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double numero;
+
+            Console.WriteLine("Dame el numero y te dire si es par o impar");
+            numero = Convert.ToDouble(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("El numero es par");
+
+            }
+            else
+            {
+                Console.WriteLine("El numero es impar");
+
+            }
+        }
+    }
+}
